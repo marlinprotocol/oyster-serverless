cfe4347f40507538788fa5c007afdd3208bd82dc
sagarparker <sagar8parker@gmail.com>
2023-03-01 13:25:00 +0530

    validation and json response

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,133 @@
 use actix_web::{get,web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize};
-use dotenv::dotenv;
-use oyster::*;
+use serde::{Deserialize,Serialize};
+use serde_json::{Value};
+use uuid::Uuid;
 use std::env;
+use dotenv::dotenv;
+use validator::{Validate};
+use oyster::*;
 
-#[derive(Deserialize)]
+
+#[derive(Debug,Validate,Deserialize)]
 struct RequestBody {
+    #[validate(length(min = 1))]
     tx_hash: String,
+}
+
+#[derive(Serialize)]
+struct JsonResponse {
+    status: String,
+    message: String,
+    data: Option<Value>
 }
 
 #[get("/serverless")]
 async fn serverless(jsonbody: web::Json<RequestBody>) -> impl Responder {
 
+    // Validation for the request json body
+    if let Err(err) = jsonbody.validate() {
+        println!("{}",err);
+        let resp = JsonResponse { 
+            status: "error".to_string(), 
+            message: "Invalid payload".to_string(), 
+            data: Some(Value::String(err.to_string()))};
+        return HttpResponse::BadRequest().json(resp)
+    }
+
     let workerd_runtime_path =  env::var("RUNTIME_PATH").expect("RUNTIME_PATH must be a valid path");
+
     let tx_hash = &jsonbody.tx_hash;
+    let file_name = tx_hash.to_string()+&Uuid::new_v4().to_string();
 
     //Fetching the transaction data using the transaction hash and decoding the calldata
-    let json_response = get_transaction_data(tx_hash).await.unwrap()["result"]["input"].to_string();
+    let json_response = get_transaction_data(&tx_hash).await.unwrap()["result"]["input"].to_string();
     let decoded_calldata = decode_call_data(&json_response);
 
     //Fetching a free port
     let free_port = get_free_port();
 
     //Generating the js and capnp file
-    let js_file = create_js_file(decoded_calldata,tx_hash,workerd_runtime_path.to_string()).await;
+    let js_file = create_js_file(decoded_calldata,&file_name,workerd_runtime_path.to_string()).await;
 
     match js_file {
-        Ok(_) => {println!("JS file generated.")},
+        Ok(_) => {println!("\nJS file generated.")},
         Err(e) =>{
+            let resp = JsonResponse { 
+                status: "error".to_string(), 
+                message: "Error generating the JS file".to_string(), 
+                data: None };
             println!("Error : {}",e);
-            return HttpResponse::InternalServerError().body("Error generating the JS file")
+            return HttpResponse::InternalServerError().json(resp)
         }
     };
 
-    let capnp_file = create_capnp_file(tx_hash,free_port,workerd_runtime_path.to_string()).await;
+    let capnp_file = create_capnp_file(&file_name,free_port,workerd_runtime_path.to_string()).await;
     
     match capnp_file {
         Ok(_) => {println!("Config file generated.")},
         Err(e) =>{
+            let resp = JsonResponse { 
+                status: "error".to_string(), 
+                message: "Error generating the configuration file".to_string(), 
+                data: None };
             println!("Error : {}",e);
-            return HttpResponse::InternalServerError().body("Error generating the config file")
+            return HttpResponse::InternalServerError().json(resp)
         }
     }
 
-    let js_file_path = workerd_runtime_path.to_string()+&tx_hash.to_string()+".js";
-    let capnp_file_path = workerd_runtime_path.to_string()+&tx_hash.to_string()+".capnp";
+    let js_file_path = workerd_runtime_path.to_string()+&file_name.to_string()+".js";
+    let capnp_file_path = workerd_runtime_path.to_string()+&file_name.to_string()+".capnp";
 
     //Run the workerd runtime with generated files
 
-    let workerd = run_workerd_runtime(tx_hash,workerd_runtime_path.to_string()).await;
+    let workerd = run_workerd_runtime(&file_name,workerd_runtime_path.to_string()).await;
     
     if workerd.is_err() == true {
         let _deleted_js_file = delete_file(&js_file_path);
         let _deleted_capnp_file = delete_file(&capnp_file_path);
         let workerd_error = workerd.err();
         println!("Error running the workerd runtime: {:?}",workerd_error);
-        return HttpResponse::InternalServerError().body("Error running the workerd runtime")
+        let resp = JsonResponse { 
+            status: "error".to_string(), 
+            message: "Error running the workerd runtime".to_string(), 
+            data: None };
+        return HttpResponse::InternalServerError().json(resp)
     }
 
     let mut workerd_process = match workerd {
         Ok(data) => data,
-        Err(e) =>{
-            println!("Error : {}",e);
-            return HttpResponse::InternalServerError().body("Error running the workerd runtime")
-        }
+        Err(e) =>{panic!("{}",e)}
     };
 
     // Wait for the port to bind
     if wait_for_port(free_port){
         let workerd_respone = get_workerd_response(free_port).await.unwrap();
-        let _kill_workerd_process = workerd_process.kill();
+        let kill_workerd_process = workerd_process.kill();
+
+        match kill_workerd_process {
+            Ok(_) => {println!("Process {} terminated", workerd_process.id())},
+            Err(_) => {println!("Error terminating the process : {}",workerd_process.id())}
+        }
         
         //Delete the generated file once the response is generated
 
         let _deleted_js_file = delete_file(&js_file_path);
         let _deleted_capnp_file = delete_file(&capnp_file_path);
     
-        HttpResponse::Ok().body(workerd_respone)
+        let resp = JsonResponse { 
+            status: "success".to_string(), 
+            message: "Response successfully generated".to_string(), 
+            data: Some(Value::String(workerd_respone.to_string())) };
+
+        println!("Generated response");
+        return HttpResponse::Ok().json(resp)
+
     }else{
-        HttpResponse::InternalServerError().body("Error in fetching resposne from the workerd runtime")
+        let resp = JsonResponse { 
+            status: "error".to_string(), 
+            message: "Failed to bind to the workerd process".to_string(), 
+            data: None };
+        return HttpResponse::InternalServerError().json(resp)
     }
 
 }
