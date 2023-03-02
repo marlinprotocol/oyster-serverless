cadb0cb732a8034b412063df5aa8630a4a554a84
sagarparker <sagar8parker@gmail.com>
2023-03-02 11:27:50 +0530

    Formating fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
-use serde_json::{json, Value};
-use reqwest::{Client, Error};
 use ethers::abi::decode;
 use ethers::abi::ParamType;
 use ethers::core::utils::hex::decode as hex_decode;
-use tokio::io::AsyncWriteExt;
-use std::net::TcpListener;
-use tokio::fs::{File};
+use reqwest::{Client, Error};
+use serde_json::{json, Value};
 use std::fs::remove_file;
-use std::process::{Command,Child};
-use std::net::{TcpStream};
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::process::{Child, Command};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 
-//Get a free port for running workerd 
+//Get a free port for running workerd
 pub fn get_free_port() -> u16 {
     let listener = TcpListener::bind("127.0.0.1:0").unwrap();
     let port = listener.local_addr().unwrap().port();
@@ -20,21 +20,18 @@ pub fn get_free_port() -> u16 {
 }
 
 //Decoding calldata using ethers
-pub fn decode_call_data(json_data:&str) -> Result<String, Box<dyn std::error::Error>> {
+pub fn decode_call_data(json_data: &str) -> Result<String, Box<dyn std::error::Error>> {
     let json_response_size = json_data.len();
-    let call_data = &json_data[11..json_response_size-1];
+    let call_data = &json_data[11..json_response_size - 1];
     let vec1 = hex_decode(call_data).unwrap();
     let data: &[u8] = vec1.as_slice();
-    let result = decode(
-        vec![ParamType::String].as_slice(),
-        data
-    ).unwrap();
+    let result = decode(vec![ParamType::String].as_slice(), data).unwrap();
     let decoded_calldata = result[0].to_string();
     Ok(decoded_calldata)
 }
 
 //Fetching the calldata using the txhash provided by the user
-pub async fn get_transaction_data(_tx_hash:&str) -> Result<Value, Error> {
+pub async fn get_transaction_data(_tx_hash: &str) -> Result<Value, Error> {
     let client = Client::new();
     let url = "https://goerli-rollup.arbitrum.io/rpc";
     let method = "eth_getTransactionByHash";
@@ -48,16 +45,13 @@ pub async fn get_transaction_data(_tx_hash:&str) -> Result<Value, Error> {
         "id": id,
     });
 
-    let response = client.post(url)
-        .json(&request)
-        .send()
-        .await?;
+    let response = client.post(url).json(&request).send().await?;
 
     let json_response = response.json::<Value>().await;
 
     match json_response {
         Ok(data) => Ok(data),
-        Err(e) => Err(e)
+        Err(e) => Err(e),
     }
 }
 
@@ -75,18 +69,22 @@ pub fn wait_for_port(port: u16) -> bool {
 }
 
 //Fetching response from the workerd runtime using a http request
-pub async fn get_workerd_response(port:u16) -> Result<String, Box<dyn std::error::Error>> {
+pub async fn get_workerd_response(port: u16) -> Result<String, Box<dyn std::error::Error>> {
     let port_str = port.to_string();
-    let req_url = "http://127.0.0.1:".to_string()+&port_str+"/";
+    let req_url = "http://127.0.0.1:".to_string() + &port_str + "/";
     let response = reqwest::get(req_url.to_string()).await?;
     let body = response.text().await?;
     Ok(body)
-} 
+}
 
 //Generating a capnp configuration file
-pub async fn create_capnp_file(tx_hash:&str,free_port:u16,workerd_runtime_path:&str)-> std::io::Result<()> {
-
-    let capnp_data = format!("using Workerd = import \"/workerd/workerd.capnp\";
+pub async fn create_capnp_file(
+    tx_hash: &str,
+    free_port: u16,
+    workerd_runtime_path: &str,
+) -> std::io::Result<()> {
+    let capnp_data = format!(
+        "using Workerd = import \"/workerd/workerd.capnp\";
 
     const helloWorldExample :Workerd.Config = (
       services = [ (name = \"main\", worker = .helloWorld) ],
@@ -96,27 +94,35 @@ pub async fn create_capnp_file(tx_hash:&str,free_port:u16,workerd_runtime_path:&
     const helloWorld :Workerd.Worker = (
       serviceWorkerScript = embed \"{tx_hash}.js\",
       compatibilityDate = \"2022-09-16\",
-    );");
-   
-    let mut file = File::create(workerd_runtime_path.to_string()+&tx_hash+".capnp").await?;
+    );"
+    );
+
+    let mut file = File::create(workerd_runtime_path.to_string() + &tx_hash + ".capnp").await?;
     file.write_all(capnp_data.as_bytes()).await?;
     Ok(())
 }
 
 //Generating the js file using the decoded calldata
-pub async fn create_js_file(decoded_calldata:&str,tx_hash:&str,workerd_runtime_path:&str)-> std::io::Result<()> {
-    let mut file = File::create(workerd_runtime_path.to_string()+&tx_hash+".js").await?;
+pub async fn create_js_file(
+    decoded_calldata: &str,
+    tx_hash: &str,
+    workerd_runtime_path: &str,
+) -> std::io::Result<()> {
+    let mut file = File::create(workerd_runtime_path.to_string() + &tx_hash + ".js").await?;
     file.write_all(decoded_calldata.as_bytes()).await?;
     Ok(())
 }
 
 //Running users js code using workerd and the generated config file
-pub async fn run_workerd_runtime(tx_hash:&str,workerd_runtime_path:&str) -> Result<Child, Box<dyn std::error::Error>>{
-    let child = Command::new(workerd_runtime_path.to_string()+"workerd")
+pub async fn run_workerd_runtime(
+    tx_hash: &str,
+    workerd_runtime_path: &str,
+) -> Result<Child, Box<dyn std::error::Error>> {
+    let child = Command::new(workerd_runtime_path.to_string() + "workerd")
         .arg("serve")
-        .arg(workerd_runtime_path.to_string()+&tx_hash+".capnp")
+        .arg(workerd_runtime_path.to_string() + &tx_hash + ".capnp")
         .spawn()?;
-        Ok(child)
+    Ok(child)
 }
 
 //Deleting files (js,capnp) once the response is generated
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use actix_web::{get,web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize,Serialize};
-use serde_json::{Value};
-use uuid::Uuid;
-use std::env;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
-use validator::{Validate};
 use oyster::*;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::env;
+use uuid::Uuid;
+use validator::Validate;
 
-
-#[derive(Debug,Validate,Deserialize)]
+#[derive(Debug, Validate, Deserialize)]
 struct RequestBody {
-    #[validate(length(min = 1),required)]
+    #[validate(length(min = 1), required)]
     tx_hash: Option<String>,
 }
 
@@ -18,57 +17,60 @@ struct RequestBody {
 struct JsonResponse {
     status: String,
     message: String,
-    data: Option<Value>
+    data: Option<Value>,
 }
 
 #[get("/serverless")]
 async fn serverless(jsonbody: web::Json<RequestBody>) -> impl Responder {
-
     // Validation for the request json body
     if let Err(err) = jsonbody.validate() {
-        println!("{}",err);
-        let resp = JsonResponse { 
-            status: "error".to_string(), 
-            message: "Invalid payload".to_string(), 
-            data: Some(Value::String(err.to_string()))};
-        return HttpResponse::BadRequest().json(resp)
+        println!("{}", err);
+        let resp = JsonResponse {
+            status: "error".to_string(),
+            message: "Invalid payload".to_string(),
+            data: Some(Value::String(err.to_string())),
+        };
+        return HttpResponse::BadRequest().json(resp);
     }
 
-    let workerd_runtime_path =  env::var("RUNTIME_PATH").expect("RUNTIME_PATH must be a valid path");
+    let workerd_runtime_path = env::var("RUNTIME_PATH").expect("RUNTIME_PATH must be a valid path");
     let tx_hash = jsonbody.tx_hash.as_ref().unwrap();
-    let file_name = tx_hash.to_string()+&Uuid::new_v4().to_string();
+    let file_name = tx_hash.to_string() + &Uuid::new_v4().to_string();
 
     //Fetching the transaction data using the transaction hash and decoding the calldata
-    // let json_response = get_transaction_data(&tx_hash).await.unwrap()["result"]["input"].to_string();
     let json_response = match get_transaction_data(&tx_hash).await {
-        Ok(data) => {data["result"]["input"].to_string()},
+        Ok(data) => data["result"]["input"].to_string(),
         Err(e) => {
-            let resp = JsonResponse { 
-                status: "error".to_string(), 
-                message: "Error fetching transacton data".to_string(), 
-                data: None };
-            println!("Error : {}",e);
-            return HttpResponse::InternalServerError().json(resp)
+            let resp = JsonResponse {
+                status: "error".to_string(),
+                message: "Error fetching transacton data".to_string(),
+                data: None,
+            };
+            println!("Error : {}", e);
+            return HttpResponse::InternalServerError().json(resp);
         }
     };
 
     if json_response == "null" {
-        let resp = JsonResponse { 
-            status: "error".to_string(), 
-            message: "Error fetching the call data, make sure a valid tx_hash is provided".to_string(), 
-            data: None };
-        return HttpResponse::BadRequest().json(resp)
+        let resp = JsonResponse {
+            status: "error".to_string(),
+            message: "Error fetching the call data, make sure a valid tx_hash is provided"
+                .to_string(),
+            data: None,
+        };
+        return HttpResponse::BadRequest().json(resp);
     }
 
-    let decoded_calldata = match decode_call_data(&json_response){
+    let decoded_calldata = match decode_call_data(&json_response) {
         Ok(data) => data,
         Err(e) => {
-            println!("{}",e);
-            let resp = JsonResponse { 
-                status: "error".to_string(), 
-                message: "Error decoding the call data".to_string(), 
-                data: None };
-            return HttpResponse::InternalServerError().json(resp)
+            println!("{}", e);
+            let resp = JsonResponse {
+                status: "error".to_string(),
+                message: "Error decoding the call data".to_string(),
+                data: None,
+            };
+            return HttpResponse::InternalServerError().json(resp);
         }
     };
 
@@ -76,102 +78,114 @@ async fn serverless(jsonbody: web::Json<RequestBody>) -> impl Responder {
     let free_port = get_free_port();
 
     //Generating the js and capnp file
-    let js_file = create_js_file(&decoded_calldata,&file_name,&workerd_runtime_path).await;
+    let js_file = create_js_file(&decoded_calldata, &file_name, &workerd_runtime_path).await;
 
     match js_file {
-        Ok(_) => {println!("\nJS file generated.")},
-        Err(e) =>{
-            let resp = JsonResponse { 
-                status: "error".to_string(), 
-                message: "Error generating the JS file".to_string(), 
-                data: None };
-            println!("Error : {}",e);
-            return HttpResponse::InternalServerError().json(resp)
+        Ok(_) => {
+            println!("\nJS file generated.")
+        }
+        Err(e) => {
+            let resp = JsonResponse {
+                status: "error".to_string(),
+                message: "Error generating the JS file".to_string(),
+                data: None,
+            };
+            println!("Error : {}", e);
+            return HttpResponse::InternalServerError().json(resp);
         }
     };
 
-    let capnp_file = create_capnp_file(&file_name,free_port,&workerd_runtime_path).await;
-    
+    let capnp_file = create_capnp_file(&file_name, free_port, &workerd_runtime_path).await;
+
     match capnp_file {
-        Ok(_) => {println!("Config file generated.")},
-        Err(e) =>{
-            let resp = JsonResponse { 
-                status: "error".to_string(), 
-                message: "Error generating the configuration file".to_string(), 
-                data: None };
-            println!("Error : {}",e);
-            return HttpResponse::InternalServerError().json(resp)
+        Ok(_) => {
+            println!("Config file generated.")
+        }
+        Err(e) => {
+            let resp = JsonResponse {
+                status: "error".to_string(),
+                message: "Error generating the configuration file".to_string(),
+                data: None,
+            };
+            println!("Error : {}", e);
+            return HttpResponse::InternalServerError().json(resp);
         }
     }
 
-    let js_file_path = workerd_runtime_path.to_string()+&file_name.to_string()+".js";
-    let capnp_file_path = workerd_runtime_path.to_string()+&file_name.to_string()+".capnp";
+    let js_file_path = workerd_runtime_path.to_string() + &file_name.to_string() + ".js";
+    let capnp_file_path = workerd_runtime_path.to_string() + &file_name.to_string() + ".capnp";
 
     //Run the workerd runtime with generated files
 
-    let workerd = run_workerd_runtime(&file_name,&workerd_runtime_path).await;
-    
+    let workerd = run_workerd_runtime(&file_name, &workerd_runtime_path).await;
+
     if workerd.is_err() == true {
         let _deleted_js_file = delete_file(&js_file_path);
         let _deleted_capnp_file = delete_file(&capnp_file_path);
         let workerd_error = workerd.err();
-        println!("Error running the workerd runtime: {:?}",workerd_error);
-        let resp = JsonResponse { 
-            status: "error".to_string(), 
-            message: "Error running the workerd runtime".to_string(), 
-            data: None };
-        return HttpResponse::InternalServerError().json(resp)
+        println!("Error running the workerd runtime: {:?}", workerd_error);
+        let resp = JsonResponse {
+            status: "error".to_string(),
+            message: "Error running the workerd runtime".to_string(),
+            data: None,
+        };
+        return HttpResponse::InternalServerError().json(resp);
     }
 
     let mut workerd_process = match workerd {
         Ok(data) => data,
-        Err(e) =>{panic!("{}",e)}
+        Err(e) => {
+            panic!("{}", e)
+        }
     };
 
     // Wait for the port to bind
-    if wait_for_port(free_port){
+    if wait_for_port(free_port) {
         let workerd_respone = get_workerd_response(free_port).await.unwrap();
         let kill_workerd_process = workerd_process.kill();
 
         match kill_workerd_process {
-            Ok(_) => {println!("Process {} terminated", workerd_process.id())},
-            Err(_) => {println!("Error terminating the process : {}",workerd_process.id())}
+            Ok(_) => {
+                println!("Process {} terminated", workerd_process.id())
+            }
+            Err(_) => {
+                println!("Error terminating the process : {}", workerd_process.id())
+            }
         }
-        
+
         //Delete the generated file once the response is generated
 
         let _deleted_js_file = delete_file(&js_file_path);
         let _deleted_capnp_file = delete_file(&capnp_file_path);
-    
-        let resp = JsonResponse { 
-            status: "success".to_string(), 
-            message: "Response successfully generated".to_string(), 
-            data: Some(Value::String(workerd_respone.to_string())) };
+
+        let resp = JsonResponse {
+            status: "success".to_string(),
+            message: "Response successfully generated".to_string(),
+            data: Some(Value::String(workerd_respone.to_string())),
+        };
 
         println!("Generated response");
-        return HttpResponse::Ok().json(resp)
-
-    }else{
-        let resp = JsonResponse { 
-            status: "error".to_string(), 
-            message: "Failed to bind to the workerd process".to_string(), 
-            data: None };
-        return HttpResponse::InternalServerError().json(resp)
+        return HttpResponse::Ok().json(resp);
+    } else {
+        let resp = JsonResponse {
+            status: "error".to_string(),
+            message: "Failed to bind to the workerd process".to_string(),
+            data: None,
+        };
+        return HttpResponse::InternalServerError().json(resp);
     }
-
 }
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let port:u16 = env::var("PORT").unwrap().parse::<u16>().expect("PORT must be a valid number");
-    let server = HttpServer::new(|| {
-        App::new()
-            .service(serverless)
-    })
-    .bind(("0.0.0.0",port))?
-    .run();
-    println!("Server started on port {}",port);
+    let port: u16 = env::var("PORT")
+        .unwrap()
+        .parse::<u16>()
+        .expect("PORT must be a valid number");
+    let server = HttpServer::new(|| App::new().service(serverless))
+        .bind(("0.0.0.0", port))?
+        .run();
+    println!("Server started on port {}", port);
     server.await
 }
