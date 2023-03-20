8e4cc6d09219e4ddab90d1e2c20072a1bda9a908
sagarparker <sagar8parker@gmail.com>
2023-03-20 15:50:56 +0530

    Added support for user input

--- src/serverless.rs
+++ src/serverless.rs
@@ -1,34 +1,20 @@
 use ethers::abi::decode;
 use ethers::abi::ParamType;
 use ethers::core::utils::hex::decode as hex_decode;
+use reqwest::Response;
 use reqwest::{Client, Error};
 use serde_json::{json, Value};
+use std::collections::HashMap;
 use std::fs;
 use std::fs::remove_file;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::process::Stdio;
 use std::process::{Child, Command};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
-
-//Get a free port for running workerd
-pub fn get_free_port() -> u16 {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-    listener.local_addr().unwrap().port()
-}
-
-//Decoding calldata using ethers
-pub fn decode_call_data(json_data: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let json_response_size = json_data.len();
-    let call_data = &json_data[11..json_response_size - 1];
-    let vec1 = hex_decode(call_data).unwrap();
-    let data: &[u8] = vec1.as_slice();
-    let result = decode(vec![ParamType::String].as_slice(), data).unwrap();
-    let decoded_calldata = result[0].to_string();
-    Ok(decoded_calldata)
-}
 
 //Fetching the calldata using the txhash provided by the user
 pub async fn get_transaction_data(_tx_hash: &str) -> Result<Value, Error> {
@@ -55,26 +41,32 @@ pub async fn get_transaction_data(_tx_hash: &str) -> Result<Value, Error> {
     }
 }
 
-//Wait for a port to bind
-pub fn wait_for_port(port: u16) -> bool {
-    let start_time = Instant::now();
-
-    while start_time.elapsed() < Duration::from_secs(1) {
-        match TcpStream::connect(format!("127.0.0.1:{}", port)) {
-            Ok(_) => return true,
-            Err(_) => sleep(Duration::from_millis(1)),
-        }
-    }
-    false
+//Decoding calldata using ethers
+pub fn decode_call_data(json_data: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let json_response_size = json_data.len();
+    let call_data = &json_data[11..json_response_size - 1];
+    let vec1 = hex_decode(call_data).unwrap();
+    let data: &[u8] = vec1.as_slice();
+    let result = decode(vec![ParamType::String].as_slice(), data).unwrap();
+    let decoded_calldata = result[0].to_string();
+    Ok(decoded_calldata)
 }
 
-//Fetching response from the workerd runtime using a http request
-pub async fn get_workerd_response(port: u16) -> Result<String, Box<dyn std::error::Error>> {
-    let port_str = port.to_string();
-    let req_url = "http://127.0.0.1:".to_string() + &port_str + "/";
-    let response = reqwest::get(req_url.to_string()).await?;
-    let body = response.text().await?;
-    Ok(body)
+//Get a free port for running workerd
+pub fn get_free_port() -> u16 {
+    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    listener.local_addr().unwrap().port()
+}
+
+//Generating the js file using the decoded calldata
+pub async fn create_js_file(
+    decoded_calldata: &str,
+    tx_hash: &str,
+    workerd_runtime_path: &str,
+) -> std::io::Result<()> {
+    let mut file = File::create(workerd_runtime_path.to_string() + tx_hash + ".js").await?;
+    file.write_all(decoded_calldata.as_bytes()).await?;
+    Ok(())
 }
 
 //Generating a capnp configuration file
@@ -102,39 +94,6 @@ pub async fn create_capnp_file(
     Ok(())
 }
 
-//Generating the js file using the decoded calldata
-pub async fn create_js_file(
-    decoded_calldata: &str,
-    tx_hash: &str,
-    workerd_runtime_path: &str,
-) -> std::io::Result<()> {
-    let mut file = File::create(workerd_runtime_path.to_string() + tx_hash + ".js").await?;
-    file.write_all(decoded_calldata.as_bytes()).await?;
-    Ok(())
-}
-
-//Running users js code using workerd and the generated config file
-pub async fn run_workerd_runtime(
-    file_name: &str,
-    workerd_runtime_path: &str,
-    available_cgroup: &str,
-) -> Result<Child, Box<dyn std::error::Error>> {
-    let child = Command::new("/usr/bin/cgexec")
-        .arg("-g")
-        .arg("memory:".to_string() + available_cgroup)
-        .arg(workerd_runtime_path.to_string() + "workerd")
-        .arg("serve")
-        .arg(workerd_runtime_path.to_string() + file_name + ".capnp")
-        .spawn()?;
-    Ok(child)
-}
-
-//Deleting files (js,capnp) once the response is generated
-pub fn delete_file(file_path: &str) -> Result<(), Error> {
-    let _remove_file = remove_file(file_path);
-    Ok(())
-}
-
 //Fetching an available cgroup from the list of cgroups generated at boot
 pub fn find_available_cgroup(cgroup_version: &str) -> Result<String, std::io::Error> {
     let cgroup_list = ["workerd1", "workerd2", "workerd3", "workerd4", "workerd5"];
@@ -154,4 +113,58 @@ pub fn find_available_cgroup(cgroup_version: &str) -> Result<String, std::io::Er
     }
 
     Ok(String::from("No available cgroup"))
+}
+
+//Running users code using workerd inside a generated cgroup
+pub async fn run_workerd_runtime(
+    file_name: &str,
+    workerd_runtime_path: &str,
+    available_cgroup: &str,
+) -> Result<Child, Box<dyn std::error::Error>> {
+    let child = Command::new("/usr/bin/cgexec")
+        .arg("-g")
+        .arg("memory:".to_string() + available_cgroup)
+        .arg(workerd_runtime_path.to_string() + "workerd")
+        .arg("serve")
+        .arg(workerd_runtime_path.to_string() + file_name + ".capnp")
+        .stdin(Stdio::piped())
+        .spawn()?;
+    Ok(child)
+}
+
+
+//Wait for a port to bind
+pub fn wait_for_port(port: u16) -> bool {
+    let start_time = Instant::now();
+
+    while start_time.elapsed() < Duration::from_secs(1) {
+        match TcpStream::connect(format!("127.0.0.1:{}", port)) {
+            Ok(_) => return true,
+            Err(_) => sleep(Duration::from_millis(1)),
+        }
+    }
+    false
+}
+
+//Fetching response from the workerd runtime using a http request
+pub async fn get_workerd_response(
+    port: u16,
+    input: Option<HashMap<String, serde_json::Value>>,
+) -> Result<Response, Box<dyn std::error::Error>> {
+    let port_str = port.to_string();
+    let req_url = "http://127.0.0.1:".to_string() + &port_str + "/";
+    let client = reqwest::Client::new();
+    let response = client
+        .post(req_url)
+        .header("Content-Type", "application/json")
+        .json(&input)
+        .send()
+        .await?;
+    Ok(response)
+}
+
+//Deleting files (js,capnp) once the response is generated
+pub fn delete_file(file_path: &str) -> Result<(), Error> {
+    let _remove_file = remove_file(file_path);
+    Ok(())
 }
