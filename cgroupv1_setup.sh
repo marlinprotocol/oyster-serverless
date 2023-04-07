89aa106be1f2b4b102cf2e00173eaa494661b071
sagarparker <sagar8parker@gmail.com>
2023-04-07 18:12:24 +0530

    Fix for user cgroups setup v1&v2

--- cgroupv1_setup.sh
+++ cgroupv1_setup.sh
@@ -3,7 +3,7 @@
 for i in $(seq 1 20)
 do
   cgcreate -g memory,cpu:workerd$i
-  echo "$cgroup_memory_size"M > /sys/fs/cgroup/memory/workerd$i/memory.limit_in_bytes
+  echo 100M > /sys/fs/cgroup/memory/workerd$i/memory.limit_in_bytes
   echo "1000000" > /sys/fs/cgroup/cpu/workerd$i/cpu.cfs_period_us
   echo "100000" > /sys/fs/cgroup/cpu/workerd$i/cpu.cfs_quota_us
 done
--- cgroupv2_setup.sh
+++ cgroupv2_setup.sh
@@ -3,6 +3,6 @@
 for i in $(seq 1 20)
 do
   cgcreate -g memory,cpu:workerd$i
-  echo "$cgroup_memory_size"M > /sys/fs/cgroup/workerd$i/memory.max
+  echo 100M > /sys/fs/cgroup/workerd$i/memory.max
   echo "100000 1000000" > /sys/fs/cgroup/workerd$i/cpu.max
 done
