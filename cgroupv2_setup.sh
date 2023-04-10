3d2fc1f610019fc1cf97d774b0b7492431997cb1
sagarparker <sagar8parker@gmail.com>
2023-04-10 17:41:16 +0530

    Update to the croups setup

--- cgroupv2_setup.sh
+++ cgroupv2_setup.sh
@@ -4,5 +4,5 @@ for i in $(seq 1 20)
 do
   cgcreate -g memory,cpu:workerd$i
   echo 100M > /sys/fs/cgroup/workerd$i/memory.max
-  echo "100000 1000000" > /sys/fs/cgroup/workerd$i/cpu.max
+  echo "50000 1000000" > /sys/fs/cgroup/workerd$i/cpu.max
 done
