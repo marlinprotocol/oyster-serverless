4a11f1b0a6a368fab091dcc95fb3dc746395533a
sagarparker <sagar8parker@gmail.com>
2023-04-25 06:20:29 +0000

    Update to the tests

--- src/tests.rs
+++ src/tests.rs
@@ -32,7 +32,7 @@ pub mod serverlesstest {
         )
         .await;
         let valid_payload = json!({
-            "tx_hash": "0xc7d9122f583971d4801747ab24cf3e83984274b8d565349ed53a73e0a547d113",
+            "code_id":"test",
             "input": {
                 "num": 10
             }
@@ -48,89 +48,7 @@ pub mod serverlesstest {
     }
 
     #[actix_web::test]
-    async fn interacting_with_wrong_smartcontract() {
-        dotenv().ok();
-        let cgroup_version: u8 = env::var("CGROUP_VERSION")
-            .unwrap()
-            .parse::<u8>()
-            .expect("CGROUP VERSION must be a valid number ( Options: 1 or 2)");
-
-        let cgroup_list = serverless::get_cgroup_list(cgroup_version).unwrap();
-        if cgroup_list.is_empty() {
-            log::error!("No cgroups found. Make sure you have set up cgroups on your system by following the instructions in the readme file.");
-            std::process::exit(1);
-        }
-
-        let app = test::init_service(
-            App::new()
-                .app_data(web::Data::new(AppState {
-                    cgroup_list: cgroup_list.clone(),
-                    cgroup_version,
-                }))
-                .configure(handler::config),
-        )
-        .await;
-
-        let invalid_payload = json!({
-            "tx_hash": "0x37b0b2d9dd58d9130781fc914da456c16ec403010e8d4c27b0ea4657a24c8546",
-            "input": {
-                "num": 10
-            }
-        });
-
-        let req = test::TestRequest::post()
-            .uri("/api/serverless")
-            .set_json(&invalid_payload)
-            .to_request();
-
-        let resp = test::call_service(&app, req).await;
-
-        assert_eq!(resp.status(), http::StatusCode::BAD_REQUEST);
-    }
-
-    #[actix_web::test]
-    async fn invalid_txhash() {
-        dotenv().ok();
-        let cgroup_version: u8 = env::var("CGROUP_VERSION")
-            .unwrap()
-            .parse::<u8>()
-            .expect("CGROUP VERSION must be a valid number ( Options: 1 or 2)");
-
-        let cgroup_list = serverless::get_cgroup_list(cgroup_version).unwrap();
-        if cgroup_list.is_empty() {
-            log::error!("No cgroups found. Make sure you have set up cgroups on your system by following the instructions in the readme file.");
-            std::process::exit(1);
-        }
-
-        let app = test::init_service(
-            App::new()
-                .app_data(web::Data::new(AppState {
-                    cgroup_list: cgroup_list.clone(),
-                    cgroup_version,
-                }))
-                .configure(handler::config),
-        )
-        .await;
-
-        let invalid_payload = json!({
-            "tx_hash": "0x37b0b2d9dd58d9130781fc914da456c16ec403010e8d4c27b0ea4657a24c85",
-            "input": {
-                "num": 10
-            }
-        });
-
-        let req = test::TestRequest::post()
-            .uri("/api/serverless")
-            .set_json(&invalid_payload)
-            .to_request();
-
-        let resp = test::call_service(&app, req).await;
-
-        assert_eq!(resp.status(), http::StatusCode::BAD_REQUEST);
-    }
-
-    #[actix_web::test]
-    async fn txhash_not_provided() {
+    async fn code_id_not_provided() {
         dotenv().ok();
         let cgroup_version: u8 = env::var("CGROUP_VERSION")
             .unwrap()
@@ -190,7 +108,7 @@ pub mod serverlesstest {
         .await;
 
         let invalid_payload = json!({
-            "tx_hash": "0x3d2deb53d077f88b40cdf3a81ce3cac6367fddce22f1f131e322e7463ce34f8f",
+            "code_id":"invalidjscode",
             "input": {
                 "num": 100
             }
@@ -231,7 +149,7 @@ pub mod serverlesstest {
         .await;
 
         let invalid_payload = json!({
-            "tx_hash": "0xc7d9122f583971d4801747ab24cf3e83984274b8d565349ed53a73e0a547d113"
+            "code_id":"test"
         });
 
         let req = test::TestRequest::post()
@@ -269,7 +187,7 @@ pub mod serverlesstest {
         .await;
 
         let invalid_payload = json!({
-            "tx_hash": "0xf17fb991c648e8bdc93f2dcfccc25c98774084ee4ae398f0b289e698b9992303"
+            "code_id":"timeouttest"
         });
 
         let req = test::TestRequest::post()
