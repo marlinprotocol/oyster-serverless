d2f0fe64c990827538bd6c8a786f88b7b551b113
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-11-26 18:09:32 +0530

    cgroups struct

--- src/cgroups.rs
+++ src/cgroups.rs
@@ -1,6 +1,20 @@
 use std::fs;
 
-pub fn get_cgroups() -> Result<Vec<String>, std::io::Error> {
+pub struct Cgroups {
+    pub free: Vec<String>,
+    pub used: Vec<String>,
+}
+
+impl Cgroups {
+    pub fn new() -> Result<Cgroups, std::io::Error> {
+        Ok(Cgroups {
+            free: get_cgroups()?,
+            used: Vec::new(),
+        })
+    }
+}
+
+fn get_cgroups() -> Result<Vec<String>, std::io::Error> {
     Ok(fs::read_dir("/sys/fs/cgroup")?
         .filter_map(|dir| {
             dir.ok().and_then(|dir| {
--- src/cgroups_retriever.rs
+++ src/cgroups_retriever.rs
@@ -3,7 +3,8 @@ use std::error::Error;
 use serverless::cgroups;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("{:?}", cgroups::get_cgroups()?);
+    let cgroups = cgroups::Cgroups::new()?;
+    println!("{:?}", cgroups.free);
 
     Ok(())
 }
