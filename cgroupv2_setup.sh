27b5a00fb07332f2d645000275a9204e24a2beba
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-11-26 17:12:52 +0530

    rename cgroups

--- cgroupv2_setup.sh
+++ cgroupv2_setup.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq 1 20)
 do
-  cgcreate -g memory,cpu:workerd$i
-  echo 100M > /sys/fs/cgroup/workerd$i/memory.max
-  echo "50000 1000000" > /sys/fs/cgroup/workerd$i/cpu.max
+  cgcreate -g memory,cpu:workerd_$i
+  echo 100M > /sys/fs/cgroup/workerd_$i/memory.max
+  echo "50000 1000000" > /sys/fs/cgroup/workerd_$i/cpu.max
 done
