790888150681777b1d2a5c31fbf90b32d46b4b16
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-11-26 17:12:44 +0530

    clean up

--- cgroupv1_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in $(seq 1 20)
-do
-  cgcreate -g memory,cpu:workerd$i
-  echo 100M > /sys/fs/cgroup/memory/workerd$i/memory.limit_in_bytes
-  echo "1000000" > /sys/fs/cgroup/cpu/workerd$i/cpu.cfs_period_us
-  echo "50000" > /sys/fs/cgroup/cpu/workerd$i/cpu.cfs_quota_us
-done
